0c56803c43ac4b1011ee9c73d2d4a9b0010ffebe
Jeevananth_S <jeevananthsakthi@gmail.com>
2026-03-25 14:02:57 +0530

    day2

---
+++ javaScript/day1/task1.js
@@ -0,0 +1,160 @@
+//camelCase
+var firstName="Jeevananth";
+console.log(firstName);
+
+var lastName="S";
+console.log(lastName);
+
+var userAge=28;
+console.log(userAge);
+
+var phoneNumber=7397598890;
+console.log(phoneNumber);
+
+var emailId="jeevananthsakthi@gmail.com"
+console.log(emailId);
+
+var isLoggedIn=true;
+console.log(isLoggedIn);
+
+var accountBalance=5000000;
+console.log(accountBalance);
+
+var cityName="Chennai";
+console.log(cityName);
+
+var courseName="FullStack";
+console.log(courseName);
+
+var isActiveUser=true;
+console.log(isActiveUser);
+
+//PascalCase
+let ProductName="Laptop";
+console.log(ProductName);
+
+let OrderId=1234;
+console.log(OrderId);
+
+let CustomerName="Jeeva";
+console.log(CustomerName);
+
+let DeliveryStatus="Success";
+console.log(DeliveryStatus);
+
+let PaymentMethod="UPI";
+console.log(PaymentMethod);
+
+let TotalAmount=100000;
+console.log(TotalAmount);
+
+let DiscountValue="5000"
+console.log(DiscountValue);
+
+let ShippingAddress="Chennai";
+console.log(ShippingAddress);
+
+let OrderDate="24-03-2026";
+console.log(OrderDate);
+
+let IsPaymentCompleted=true;
+console.log(IsPaymentCompleted);
+
+//snake_case
+const user_name="Jeevananth";
+console.log(user_name);
+
+const user_age=28;
+console.log(user_age);
+
+const phone_number="7397598890";
+console.log(phone_number);
+
+const email_address="jeevananthsakthi@gmail.com";
+console.log(email_address);
+
+const account_balance=10000000;
+console.log(account_balance);
+
+const product_name="Car";
+console.log(product_name);
+
+const product_price=1000000;
+console.log(product_price);
+
+const discount_price=200000;
+console.log(discount_price);
+
+const total_amount=800000;
+console.log(total_amount);
+
+const payment_status="Success";
+console.log(payment_status);
+
+const order_id=1234;
+console.log(order_id);
+
+const order_date="24-03-2026";
+console.log(order_date);
+
+const delivery_status="Shipped";
+console.log(delivery_status);
+
+const shipping_address="Chennai";
+console.log(shipping_address);
+
+const vehicle_type="SUV";
+console.log(vehicle_type);
+
+const service_type="Wash";
+console.log(service_type);
+
+const service_cost=2000;
+console.log(service_cost);
+
+const estimated_time="30 minutes";
+console.log(estimated_time);
+
+const is_service_active=true;
+console.log(is_service_active);
+
+const support_number="180012341234";
+console.log(support_number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
