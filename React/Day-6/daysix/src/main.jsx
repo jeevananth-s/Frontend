8b465dfe501cc85fad6ddd1de241b9b59d517e02
Jeevananth_S <jeevananthsakthi@gmail.com>
2026-05-19 00:07:14 +0530

    taskupdate

--- React/Day-6/daysix/src/main.jsx
+++ React/Day-6/daysix/src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import Login from './Login.jsx'
+import ShowHide from './ShowHide.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
+    <Login/>
+    <ShowHide/>
   </StrictMode>,
 )
