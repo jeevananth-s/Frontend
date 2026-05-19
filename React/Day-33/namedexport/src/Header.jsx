9b8e97b0979e44d83c48f0cd6a77d87d977094d6
Jeevananth_S <jeevananthsakthi@gmail.com>
2026-05-19 12:20:44 +0530

    unsubmittedtask

---
+++ React/Day-33/namedexport/src/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <div>
+            <h1>Welcome to React JSX</h1>
+        </div>
+    );
+}
---
+++ React/Day-33/namedexport/src/Main.jsx
@@ -0,0 +1,8 @@
+export function Main() {
+    return (
+        <div>
+            <h2>Main Component</h2>
+            <p>This is the Main Section</p>
+        </div>
+    );
+}
